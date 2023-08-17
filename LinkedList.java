6a24a8b779ad271afdc27ec02b6b14d75a12e1d9
D1m11a <128309725+D1m11a@users.noreply.github.com>
2023-08-17 13:22:27 +0300

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,69 @@
+package LinkedList;
+
+public class LinkedList<T> {
+
+    private Node<T> firstNode;
+    private Node<T> lastNode;
+
+    private int size;
+
+    public LinkedList() {
+        size = 0;
+    }
+
+    public void add(T value) {
+        if (firstNode == null) {
+            firstNode = new Node<>(null, null, value);
+            lastNode = firstNode;
+        } else {
+            Node<T> node = new Node<T>(lastNode, null, value);
+            lastNode.setNextNode(node);
+            lastNode = node;
+        }
+        size++;
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+        if (index == 0) {
+            firstNode = firstNode.getNextNode();
+            if (firstNode == null) {
+                lastNode = null;
+            }
+        } else {
+            Node<T> previousNode = get(index - 1);
+            Node<T> nodeToRemove = previousNode.getNextNode();
+            Node<T> nextNode = nodeToRemove.getNextNode();
+            previousNode.setNextNode(nextNode);
+
+            if (nodeToRemove == lastNode) {
+                lastNode = previousNode;
+            }
+        }
+        size--;
+    }
+
+    public Node<T> get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+
+        Node<T> currentNode = firstNode;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.getNextNode();
+        }
+        return currentNode;
+    }
+
+    public void clear() {
+        firstNode = null;
+        lastNode = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+}
